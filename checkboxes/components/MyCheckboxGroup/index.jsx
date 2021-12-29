43419c7d6581f68a3f709da63694068773c5b96f
selfdialog <1483279495@qq.com>
2021-12-29 17:11:26 +0800

    update checkGroup render

--- checkboxes/components/MyCheckboxGroup/index.jsx
+++ checkboxes/components/MyCheckboxGroup/index.jsx
@@ -1,7 +1,7 @@
 import "./index.scss"
 import MyCheckbox from "../MyCheckbox"
 import MyButton from "../MyButton"
-const { useState, useCallback, useMemo } = React
+const { useState, useCallback, useMemo, useEffect } = React
 function MyCheckboxGroup({
   children, // data
   allCheckedTexts, // 全选文字说明
@@ -11,16 +11,74 @@ function MyCheckboxGroup({
   const [originData, setOriginData] = useState(children)
   const [checkedData, setCheckedData] = useState([])
   const dataProps = Object.keys(children[0])
-  const doAllCheck = useCallback((e) => {})
-  const doSingleChange = useCallback((e) => {})
-  const removeItem = useCallback((e) => {})
+
+  const doAllCheck = useCallback((e) => {
+    const checked=e.target.checked;
+    setCheckedData(checked?originData:[])
+  })
+
+  const doSingleChange = useCallback((e,item) => {
+    const checked=e.target.checked;
+    setCheckedData(checkedData=>{
+      return checked?[...checkedData,item]:checkedData.filter(data=>data.id!==item.id)
+    })
+  })
+
+  const removeItem = useCallback((id) => {
+    setOriginData(originData=>originData.filter(item=>item.id!==id));
+    setCheckedData(checkedData=>checkedData.filter(item=>item.id!==id));
+  })
+
   const allCheckedText = useMemo(() => {
     if (checkedData.length >= 0 && checkedData.length < originData.length) {
       return allCheckedTexts.all || "Check all"
     } else {
-      return allCheckedTexts.none || "Check none"
+      return allCheckedTexts.none || "Cancel all"
     }
   }, [checkedData])
+
+  useEffect(() => {
+    onCheckedDataChange(checkedData)
+  },[checkedData])
+
+  const getTd = (cells) => {
+    const arr = [];
+    cells.map(item => {
+      arr.push(<th key={item}>{item}</th>);
+    });
+
+    console.log(arr);
+
+    return arr;
+  }
+
+  const getOriginTrTD=(originData) => {
+    let arr=[]
+    originData.map(item => {
+      arr.push(
+        <tr key={item.id}>
+        <td>
+          <MyCheckbox
+            checked={checkedData.some(data => data.id===item.id)}
+            onChange={(e) => doSingleChange(e,item)}
+          ></MyCheckbox>
+        </td>
+        {
+          dataProps.map(prop => {
+            <td key={prop}>{item[prop]}</td>
+          })
+        }
+        <td>
+          <MyButton
+            onClick={()=>removeItem(item.id)}
+          >删除</MyButton>
+        </td>
+      </tr>
+      )
+    })
+    return arr;
+  }
+
   return (
     <div>
       <table border="1" width="500" align="center">
@@ -36,37 +94,23 @@ function MyCheckboxGroup({
             </td>
           </tr>
           <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+          <tr>
             {
-              headerCells.map(item => {
-                <th key={item}>{item}</th>
-              })
+              getTd(headerCells)
             }
           </tr>
         </thead>
         <tbody>
-            {
-              originData.map(item => {
-                <tr key={item.id}>
-                  <td>
-                    <MyCheckbox
-                      checked={checkedData.some(data => data.id===item.id)}
-                      onChange={(e) => doSingleChange(e,item)}
-                    ></MyCheckbox>
-                  </td>
-                  {
-                    dataProps.map(prop => {
-                      <td key={prop}>{item[prop]}</td>
-                    })
-                  }
-                  <td>
-                    <MyButton
-                      onClick={()=>removeItem(item.id)}
-                    >删除</MyButton>
-                  </td>
-                </tr>
-              })
-            }
-          </tbody>
+          {
+            getOriginTrTD(originData)
+          }
+        </tbody>
       </table>
     </div>
   )
