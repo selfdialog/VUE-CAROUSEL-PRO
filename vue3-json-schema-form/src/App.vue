432ef5f5b6e3d338912b911bd427b68a03821c7b
selfdialog <1483279495@qq.com>
2022-04-08 00:30:32 +0800

    setup运用

--- vue3-json-schema-form/src/App.vue
+++ vue3-json-schema-form/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="12"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>{{name}}:{{name2}}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,PropType } from "vue";
+import { defineComponent,PropType, reactive, ref, computed,watchEffect } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
@@ -14,6 +15,35 @@ export default defineComponent({
   components: {
     HelloWorld,
   },
+  setup(props, { slots, attrs, emit}){
+    // let name = 'jokcy'
+    /* const state=reactive({
+      name:'jokcy'
+    }) */
+
+    // {value: xxx}
+    const nameRef = ref('jokcy')
+    
+    
+    setInterval(() => {
+      // state.name += '1'
+      nameRef.value += '1'
+    },1000)
+
+    const computedNameRef = computed(() => {
+      return (nameRef.value + '2')
+    })
+
+    watchEffect(() => {
+      // 监测变化的值
+      console.log(nameRef.value)
+    })
+
+    return {
+      name: nameRef,
+      name2: computedNameRef,
+    }
+  }
 });
 </script>
 
--- vue3-json-schema-form/src/main.ts
+++ vue3-json-schema-form/src/main.ts
@@ -1,21 +1,21 @@
 
-import { createApp, defineComponent, h } from "vue";
+import { createApp, defineComponent, createVNode } from "vue";
 import  HelloWorld from './components/HelloWorld.vue';
-// import App from "./App.vue";
+import App from "./App.vue";
 const img = require('./assets/logo.png') // eslint-disable-line
-const App = defineComponent({
+/* const App = defineComponent({
   render(){
-    return h('div', {id: 'app'},[
-      h('img',{
+    return createVNode('div', {id: 'app'},[
+      createVNode('img',{
         alt: 'Vue logo',
         src: img
       }),
-      h(HelloWorld, {
+      createVNode(HelloWorld, {
         msg: 'Welcome to Your Vue.js + TypeScript App',
         age: 12
       })
     ])
   }
-})
+}) */
 
 createApp(App).mount("#app");
